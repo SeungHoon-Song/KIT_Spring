c313ab861d990672cfd5b6b50ba4556337d71ae6
SeungHoon-Song <s10269@naver.com>
2021-05-28 02:20:37 +0900

    spring upload thumbnail

--- workspace/ex04/src/main/java/com/koreait/controller/UploadController.java
+++ workspace/ex04/src/main/java/com/koreait/controller/UploadController.java
@@ -1,18 +1,33 @@
 package com.koreait.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.koreait.domain.AllFileDTO;
+import com.koreait.domain.AttachFileDTO;
+
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -39,7 +54,7 @@ public class UploadController {
 			//원하는 데이터를 메소드로 가져온다.
 			log.info("===========");
 			log.info("업로드 파일 명 : " + multipartFile.getOriginalFilename());
-			log.info("업로드 파일 크기 : " + multipartFile.getSize());
+			log.info("업로드 파일 크기 : " + multipartFile.getSize());	
 			
 				//전체 경로를 File객체에 담아준다.
 				File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
@@ -52,13 +67,18 @@ public class UploadController {
 		}
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxAction(MultipartFile[] uploadFile) {
-		log.info("upload Ajax post...");
+	@PostMapping(value="/uploadAjaxAction", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<AllFileDTO> uploadAjaxAction(MultipartFile[] uploadFile) {
+		log.info("upload Ajax post...");		
 		
 		String uploadFolder = "C:\\upload";
 		//사용자가 업로드를 한 시간인 년, 월, 일을 디렉터리로 만드는 getFolder()를 사용한다.
-		File uploadPath = new File(uploadFolder, getFolder());
+		String uploadFolderPath = getFolder();
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
+		AllFileDTO allFile = new AllFileDTO();
+		List<AttachFileDTO> succeedList = new ArrayList<>();
+		List<AttachFileDTO> failureList = new ArrayList<>();
 		
 		//만약 해당 디렉터리가 존재하지 않으면
 		if(!uploadPath.exists()) {
@@ -69,7 +89,8 @@ public class UploadController {
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("===========");
 			log.info("업로드 파일 명 : " + multipartFile.getOriginalFilename());
-			log.info("업로드 파일 크기 : " + multipartFile.getSize());
+			log.info("업로드 파일 크기 : " + multipartFile.getSize());		
+			AttachFileDTO attachDTO = new AttachFileDTO();
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			//IE에서는 파일 이름만 가져오지 않고 전체 경로를 가져오기 때문에 마지막에 위치한 파일 이름만 가져오도록 한다.
@@ -77,19 +98,62 @@ public class UploadController {
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			
 			log.info("실제 파일명: " + uploadFileName);
-			
-			//랜던함 UUID를 담아놓는다.
+			attachDTO.setFileName(uploadFileName);
+			//랜덤한 UUID를 담아놓는다.
 			UUID uuid = UUID.randomUUID();
 			//파일 이름이 중복되더라도 이름 앞에 UUID를 붙여주기 때문에 중복될 가능성이 희박하다.
 			//덮어씌워지는 것을 방지한다.
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
-			File saveFile = new File(uploadPath, uploadFileName);
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				InputStream in = new FileInputStream(saveFile);	
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
+				
+				if(checkImg(saveFile)) {
+					attachDTO.setImage(true);
+					//Stream은 파일을 통신할 때 byte가 이동할 경로이다.
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					//사용자가 첨부한 파일은 multipartFile을 통해서 가져오고,
+					//원하는 width, height를 지정한 후 변경된 이미지 파일을 FileOutputStream 객체를 통해서 업로드한다.
+					//즉, Thumbnailator는 중간관리의 역할을 한다.		multipartFile.getInputStream()
+					Thumbnailator.createThumbnail(in, thumbnail, 100, 100);
+					thumbnail.close();
+				}
+				succeedList.add(attachDTO);
 			} catch (Exception e) {
-				log.error(e.getMessage());;
+				failureList.add(attachDTO);
+				log.error(e.getMessage());
 			}
 		}
+		allFile.setSucceedList(succeedList);
+		allFile.setFailureList(failureList);
+		
+		return new ResponseEntity<AllFileDTO>(allFile, HttpStatus.OK);
+	}
+	
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName){
+		log.info("fileName: " + fileName);
+		File file = new File("C:\\upload\\" + fileName);
+		log.info("file: " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		HttpHeaders header = new HttpHeaders();
+		
+		try {
+			//헤더에 적절한 파일의 타입을 probeContentType을 통하여 포함시킨다.
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 	private String getFolder() {
@@ -105,20 +169,3 @@ public class UploadController {
 		return Files.probeContentType(file.toPath()).startsWith("image");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- workspace/ex04/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ workspace/ex04/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -5,27 +5,71 @@
 <head>
 <meta charset="UTF-8">
 <title>File Upload - Ajax</title>
+<style>
+	.uploadResult {
+		width: 100%;
+	}
+	
+	.uploadResult ul {
+		display: flex;
+		justify-content: center;
+	}
+	
+	.uploadResult ul li {
+		list-style: none;
+		padding: 10px;
+	}
+	
+</style>
 </head>
 <body>
 	<h1>File Upload - Ajax</h1>
 	<div class="uploadDiv">
 		<input type="file" name="uploadFile" multiple>
 	</div>
+	<div class="uploadResult">
+		<h3>가져오기 성공한 파일</h3>
+		<ul></ul>
+	</div>
 	<button id="uploadBtn">upload</button>
+	<div class="uploadFail" style="display:none;">
+		<h3>가져오기 실패한 파일</h3>
+		<h2>지원하지 않는 형식입니다.</h2>
+		<ul></ul>
+	</div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script>
 	$(document).ready(function(){
 		var contextPath = "${pageContext.request.contextPath}";
+		var uploadResult = $(".uploadResult ul");
+		var uploadFail = $(".uploadFail ul");
+		var cloneObj = $(".uploadDiv").clone();
+		
+		function showUploadFile(uploadResults, tag){
+			str = "";
+			$(uploadResults).each(function(i, obj){
+				if(!obj.image){
+					str += "<li><img src='/upload/resources/img/attach.png'>"+ obj.fileName +"</li>";
+				}else{
+					//encodeURIComponent("문자열값")
+					//get방식으로 전송 시 파라미터로 전달할 때, 값에 인식할 수 없는 문자가 있을 경우 쿼리 스트링 문법에 맞게 변경해야 한다.
+					//이 때 사용하는 메소드이다.
+					var fileCallPath = encodeURIComponent(obj.uploadPath + "/s_" + obj.uuid + "_" + obj.fileName);
+					str += "<li><img src='/upload/display?fileName=" + fileCallPath +"'>" + obj.fileName + "</li>";
+				}
+			})
+			$(tag).append(str);
+		}
 		
 		function check(fileName, fileSize){
 			var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
-			var maxSize = 5242880;	//5MB
+			var maxSize = 5242880; //5MB
 			if(regex.test(fileName)){
 				alert("업로드 할 수 없는 파일의 형식입니다.");
 				return false;
 			}
-			if(fileSize >= maxSize){
+			if(fileSize > maxSize){
 				alert("파일 사이즈 초과");
 				return false;
 			}
@@ -49,11 +93,20 @@
 			$.ajax({
 				url: contextPath + "/uploadAjaxAction",
 				processData: false,
-				contentType: false,
+				contentType: false,	//직접 주지않아도 돼서
 				data: formData,
 				type: "post",
+				dataType: "json",
 				success: function(result){
-					alert("Uploaded!");
+					console.log(result);
+					showUploadFile(result.succeedList, uploadResult);
+					if(result.failureList.length != 0){
+						showUploadFile(result.failureList, uploadFail);
+						$(".uploadFail").show();
+					}else{
+						$(".uploadFail").hide();
+					}
+					$(".uploadDiv").html(cloneObj.html());
 				}
 			});
 		});
